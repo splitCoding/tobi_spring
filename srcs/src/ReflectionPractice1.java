3520a821077ab937fe2849e4aa1d95bfdee6a9f0
splitube <voicesplit@daum.net>
2022-11-29 21:26:04 +0900

    리플렉션 예제1

---
+++ srcs/src/ReflectionPractice1.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Method;
+
+public class ReflectionPractice1 {
+    public static void main(String[] args) {
+        try{
+            Class find = Class.forName("java.lang.Math");
+            Method[] methods = find.getDeclaredMethods();
+            for (Method method : methods) {
+                System.out.println(find +".method = " + method);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
+class java.lang.Math.method = public static float java.lang.Math.abs(float)
+class java.lang.Math.method = public static long java.lang.Math.abs(long)
+...
+class java.lang.Math.method = static double java.lang.Math.powerOfTwoD(int)
+class java.lang.Math.method = static float java.lang.Math.powerOfTwoF(int)
+ */
