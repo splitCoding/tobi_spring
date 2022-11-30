e4e154930aefb255a905235857a2e7a0b1b52888
splitube <voicesplit@daum.net>
2022-11-30 22:39:54 +0900

    feat(리플렉션): 예제 6번

---
+++ srcs/src/ReflectionPractice6.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Constructor;
+
+//새로운 객체 생성하기
+
+public class ReflectionPractice6 {
+    public static void main(String[] args) {
+        try {
+            Class find = Class.forName("ReflectionPractice1");
+            Constructor[] constructors = find.getDeclaredConstructors();
+            for (Constructor constructor : constructors) {
+                int parameterCount = constructor.getParameterCount();
+                //인자가 없는 생성자만 사용
+                if(parameterCount == 0){
+                    Object o = constructor.newInstance();
+                    System.out.println("o.getClass() = " + o.getClass());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
+o.getClass() = class ReflectionPractice1
+*/
