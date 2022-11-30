8f858a1e5d53090189a2df9b9b8d5af1dd93662b
splitube <voicesplit@daum.net>
2022-11-30 21:29:26 +0900

    feat(리플렉션): 예제5번

--- srcs/src/ReflectionPractice4.java
+++ srcs/src/ReflectionPractice4.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
+//객체 이름으로 필드 조회하기
 public class ReflectionPractice4 {
     public static void main(String[] args) {
         try {
---
+++ srcs/src/ReflectionPractice5.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Method;
+
+//메서드 정보로 메서드 호출하기
+
+public class ReflectionPractice5 {
+    public static void main(String[] args) {
+        try {
+            Class find = Class.forName("ReflectionPractice5$FindMe");
+            Method add = find.getMethod("add", new Class[]{Integer.class, Integer.class});
+            Object addResult = add.invoke(new FindMe(), new Object[]{1, 20});
+            System.out.println("addResult = " + addResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class FindMe {
+        public int add(Integer a, Integer b) {
+            return a + b;
+        }
+    }
+}
+
+/*
+addResult = 21
+*/
