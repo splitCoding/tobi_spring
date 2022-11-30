741807ade461526cedec0d147919b5b9370aeea3
splitube <voicesplit@daum.net>
2022-11-30 22:51:04 +0900

    feat(리플렉션): 예제 7번

---
+++ srcs/src/ReflectionPractice7.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+//필드의 값을 변경하기
+
+public class ReflectionPractice7 {
+    public static void main(String[] args) {
+        try {
+            Class find = Class.forName("ReflectionPractice7$UpdateMe");
+            Field field1 = find.getDeclaredField("field1");
+            Constructor constructor = find.getDeclaredConstructor();
+            Object updateMe = constructor.newInstance();
+            System.out.println("before field1 = " + ((UpdateMe) updateMe).field1);
+            field1.set(updateMe, "Change");
+            System.out.println("after field1 = " + ((UpdateMe) updateMe).field1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class UpdateMe {
+        String field1 = "default";
+    }
+}
+
+/*
+updateMe.field1 = default
+updateMe.field1 = Change
+*/
